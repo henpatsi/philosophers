b4782d6aa34fdb6a2d8be5f19738341945576dfd
Henri Patsi <hpatsi@student.hive.fi>
2024-03-07 09:26:46 +0200

    fixed eat count to only increment after eating

--- philo/srcs/philo_actions.c
+++ philo/srcs/philo_actions.c
@@ -6,7 +6,7 @@
 /*   By: hpatsi <hpatsi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 14:48:37 by hpatsi            #+#    #+#             */
-/*   Updated: 2024/03/05 12:55:41 by hpatsi           ###   ########.fr       */
+/*   Updated: 2024/03/07 09:23:46 by hpatsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 int	philo_eat(t_thread_input input)
 {
 	set_philo_state(&input, EAT);
-	increment_philo_eat_count(input.philo, input.philo_mutex);
 	set_philo_last_eat(input.philo, input.philo_mutex);
 	if (better_sleep(input, input.args.eat_time) == -1)
 		return (-1);
 	put_down_forks(input);
+	increment_philo_eat_count(input.philo, input.philo_mutex);
 	return (1);
 }
 
